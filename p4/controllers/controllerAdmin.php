d8c63227ce341c41ecda9a95362d204c6d321cf8
maqsse <32159805+maqsse@users.noreply.github.com>
2018-02-12 09:59:07 +0100

    Update controllerAdmin.php

--- p4/controllers/controllerAdmin.php
+++ p4/controllers/controllerAdmin.php
@@ -2,70 +2,72 @@
 <?php
 session_start();
 include ("../config/Database.php");
-
+ 
 include ("../models/Comment.php");
-
+ 
 include ("../models/Billet.php");
-
+ 
 include ("../models/Admin.php");
-
+ 
 $con = new Database;
 $con->connect();
 $administrateur= new Admin; 
 $administrateur = Admin::Administration();
-
-
+ 
+ 
+ 
+ 
+ 
 // vérification des identifiants
-if (isset($_SESSION['pseudo']) && $_SESSION['pseudo'] == $administrateur->pseudo) 
+if (!isset($_SESSION['pseudo']) && $_SESSION['pseudo'] !== $administrateur->pseudo) 
 {
-}
-else{
 header("location:../racine.php?url=login");
-} 
-
-
-
+}
+ 
+ 
+ 
+ 
 // affichage des commentaires récemment signalés
-
+ 
 $com = new Comment;
 $com = Comment::affichSignal();
-
+ 
 // Si on appuie sur le bouton supprimer le commentaire est supprimé (aprés confirmation)
-
+ 
 if (isset($_GET['action']) && $_GET['action'] == 'del')
 	{
 	$com = Comment::deleteComment();
 	header("location:../racine.php?url=admin");
 	}
-
+ 
 // Affichage des billets
-
+ 
 $ticket = new Billet;
 $ticket = Billet::getListe();
-
+ 
 // Si on clique sur supprimer le billet est supprimé (aprés confirmation)
-
+ 
 if (isset($_GET['action']) && $_GET['action'] == 'delete')
 	{
 	$ticket = Billet::deleteBillet();
 	header("location:../racine.php?url=admin");
 	}
-
+ 
 // Si on a cliqué sur mise à jour le billet est mis à jour
-
+ 
 if (isset($_GET["action"]) == "maj")
 	{
 	$resultats = Billet::majBillet();
 	header("location:../racine.php?url=admin");
 	}
-
+ 
 // Si on clique sur le bouton envoyer à la suite de l'écriture d'un billet il est inséré dans la base de données
-
+ 
 if (isset($_POST["submit"]))
 	{
 	$ticket = Billet::insertBillet();
-	
+ 
 	}
-
+ 
 require ("../vues/admin.php");
-
+ 

8419a429e68dbc5a4d9c930780155b428248078d
maqsse <32159805+maqsse@users.noreply.github.com>
2018-02-12 09:56:50 +0100

    Update racine.php

--- p4/public/racine.php
+++ p4/public/racine.php
@@ -1,75 +1,87 @@
 	<!-- ROUTEUR ou ( controleur frontal) -->
+ 
 	<?php
+session_start();
 $title = "accueil";
-
-
+ 
+ 
 // Autoloader permettant de charger les différentes classes nécessaires à la page.
-
+ 
 function autoload($name)
 	{
 	if (file_exists($file = '../config/' . $name . '.php'))
 		{
 		require $file;
-
+ 
 		}
 	elseif (file_exists($file2 = '../models/' . $name . '.php'))
 		{
 		require $file2;
-
+ 
 		}
 	}
-
+ 
 spl_autoload_register('autoload');
-
+ 
 //  affichage des derniers billets
-
+ 
 if (isset($_GET['url']))
 	{
 	if ($_GET['url'] == 'readbillets')
 		{
 		include ("../controllers/controllerReadBillets.php");
-
+ 
 		}
-
+ 
 	if ($_GET['url'] == 'login')
 		{
 		include ("../controllers/controllerLogin.php");
-
+ 
 		}
 	elseif ($_GET['url'] == 'admin')
 		{
 		include ("../controllers/controllerAdmin.php");
-
+ 
 		}
 	elseif ($_GET['url'] == 'maj')
 		{
-
+ 
 		include ("../controllers/controllerMaj.php");
-
+ 
 		}
 elseif ($_GET['url'] == 'try_login')
 		{
 		include ("../controllers/controllerLogin.php");
-
+ 
 		}
-
+ 
 elseif ($_GET['url'] == 'add_post')
 		{
 		include ("../controllers/controllerMaj.php");
-
+ 
+		}
+elseif ($_GET['url'] == 'mentions')
+		{
+		include ("../vues/mentions.php");
+ 
+		}
+elseif ($_GET['url'] == 'disconnect')
+		{
+		session_destroy();
+header('location:../racine.php');
 		}
 	}
   else
 	{
 	include ("../controllers/controllerIndex.php");
-
+ 
 	}
-
+ 
 include ('../template/footer.php');
-
+ 
 ?>
-	
-
+ 
+ 
 
 
 

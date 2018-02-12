158dbcbb646bb20a496efce746dddad820b92bcc
maqsse <32159805+maqsse@users.noreply.github.com>
2018-02-12 10:00:06 +0100

    Update controllerLogin.php

--- p4/controllers/controllerLogin.php
+++ p4/controllers/controllerLogin.php
@@ -1,34 +1,34 @@
 <!-- vérif du login pour se connecter au panneau d'administration-->
 <?php
-session_start();
+//session_start();
 include ("../config/Database.php");
-
+ 
 include ("../models/Admin.php");
-
+ 
 $con = new Database;
 $con->connect();
 $administrateur = new Admin;
 $administrateur = Admin::Administration();
-
+ 
+ 
 if (isset($_POST["submit"]))
 	{
-
+ 
 	$pseudo = $_POST['pseudo'];
 	$mdp = $_POST['password'];
-	
 	if ($administrateur->pseudo == $pseudo and $administrateur->mdp ==sha1($mdp))
 		{
-		$_SESSION['pseudo'] = $administrateur->pseudo;
-        	$_SESSION['mdp'] = $administrateur->mdp;
+$_SESSION['pseudo'] = $administrateur->pseudo;
+$_SESSION['mdp'] = $administrateur->mdp;
 		header('location:../racine.php?url=admin');
 		}
 	  else
 		{
-
+ 
 		header('location:../racine.php?url=login');
 		}
 	}
-
+ 
 require ("../vues/login.php");
-
+ 
 ?>	
